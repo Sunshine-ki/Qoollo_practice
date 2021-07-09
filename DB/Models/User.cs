1bf5f62d02d02d5a841efca47b88bd7502ba8300
Matt-MS <65306309+Matt-MS@users.noreply.github.com>
2021-07-09 17:19:07 +0300

    Update User.cs

--- DB/Models/User.cs
+++ DB/Models/User.cs
@@ -6,7 +6,8 @@ namespace QoolloSSO.backend.DataBase.Models
 {
 	public class User
 	{
-		[BsonId] public string Id { get; set; }
+		[BsonId]
+		public string Id { get; set; }
 		public string Name { get; set; }
 		public string Surname { get; set; }
 		public int Age { get; set; }
