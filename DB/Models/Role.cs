810073d92d653e6023b55d7da07ad2b27d1a8d62
Matt-MS <65306309+Matt-MS@users.noreply.github.com>
2021-07-09 18:07:14 +0300

    Update Role.cs

--- DB/Models/Role.cs
+++ DB/Models/Role.cs
@@ -4,11 +4,11 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace QoolloSSO.backend.DataBase.Models
 {
-    public class Role
-    {
-        [BsonId]
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public List<string> Users { get; set; }
-    }
+	public class Role
+	{
+		[BsonId]
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public List<string> Users { get; set; }
+	}
 }
