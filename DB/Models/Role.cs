88b5af08b0855573256be9c7bc419f4c097202e6
Matt-MS <65306309+Matt-MS@users.noreply.github.com>
2021-07-09 17:19:43 +0300

    Update Role.cs

--- DB/Models/Role.cs
+++ DB/Models/Role.cs
@@ -6,7 +6,8 @@ namespace QoolloSSO.backend.DataBase.Models
 {
     public class Role
     {
-        [BsonId] public string Id { get; set; }
+        [BsonId]
+        public string Id { get; set; }
         public string Name { get; set; }
         public List<string> Users { get; set; }
     }
